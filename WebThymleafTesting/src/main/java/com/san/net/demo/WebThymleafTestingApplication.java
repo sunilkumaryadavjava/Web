2e67f55d2dd0344c9c6c1d07df51ee8c6cf26505
dell <dell@dell-PC>
2020-05-13 12:06:34 +0530

    spring boot thymleaf project testing

---
+++ WebThymleafTesting/src/main/java/com/san/net/demo/WebThymleafTestingApplication.java
@@ -0,0 +1,13 @@
+package com.san.net.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebThymleafTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebThymleafTestingApplication.class, args);
+	}
+
+}
